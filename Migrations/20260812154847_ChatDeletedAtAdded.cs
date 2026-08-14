a05345627f8ffb086c461e9df49aaa8b9c2d9829
imeanmybabyboy <pushenko.roman@gmail.com>
2026-08-14 17:08:12 +0300

    Private posts update

---
+++ Migrations/20260812154847_ChatDeletedAtAdded.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChatDeletedAtAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Chats",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Chats");
+        }
+    }
+}
